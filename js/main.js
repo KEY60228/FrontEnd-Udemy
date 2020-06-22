ecaffcfa713612c2c45813252600ac1b4bba3dbd
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-06-22 14:48:07 +0900

    finished part.66 (JS grammar)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,98 @@
-/*
-console.log('Hello World! form console')
-
-let p: HTMLElement = document.getElementById('js-div');
-
-p.addEventListener('click', function() {
-  p.textContent = '！';
-});
-*/ 
+'use strict';
+{
+    var hello = function (name, age) {
+        console.log('Hello, ' + name + '(' + age + ')');
+    };
+    var name_1 = "Taro";
+    var age = 25;
+    hello(name_1, age);
+}
+{
+    var person = {
+        name: ['Taro', 'Jiro', 'Subro'],
+        birth: 1994,
+        interests: {
+            sports: 'football',
+            music: 'rock'
+        },
+        getAllName: function () {
+            for (var _i = 0, _a = this.name; _i < _a.length; _i++) {
+                var i = _a[_i];
+                console.log(i, this.name[i]);
+            }
+        }
+    };
+    console.log(person.birth);
+    person.getAllName();
+}
+{
+    var todos = [
+        {
+            id: 1,
+            title: 'working',
+            completed: true
+        },
+        {
+            id: 2,
+            title: 'study',
+            completed: false
+        },
+        {
+            id: 3,
+            title: 'shopping',
+            completed: true
+        }
+    ];
+    for (var i in todos) {
+        var todo = todos[i];
+        if (todo.completed === true) {
+            console.log(todo.id, todo.title);
+        }
+    }
+}
+{
+    var hello = function (callback) {
+        console.log('hello ' + callback());
+    };
+    var getName = function () {
+        return 'Ichiro';
+    };
+    hello(getName);
+}
+{
+    var arry = ['Ichiro', 'Suzuki', 'Toyota', 'Nissan'];
+    arry.forEach(function (val, i, ary) {
+        console.log(val);
+    });
+}
+{
+    var arry = [1, 2, 3, 4, 5];
+    arry.reduce(function (accu, curr) {
+        console.log(accu, curr);
+        return accu + curr;
+    });
+    var str = 'animation';
+    var strArry = str.split('');
+    console.log(strArry);
+    var result = strArry.reduce(function (accu, curr) {
+        return accu + "<" + curr + ">";
+    }, '');
+    console.log(result);
+}
+{
+    var str = 'animation';
+    var strArry = str.split('');
+    var tag = function (accu, curr) {
+        return accu + "<" + curr + ">";
+    };
+    var reduce = function (arry, callback, defaultValue) {
+        var accu = defaultValue;
+        for (var i = 0; i < arry.length; i++) {
+            var curr = arry[i];
+            accu = callback(accu, curr);
+        }
+        return accu;
+    };
+    var result = reduce(strArry, tag, "");
+    console.log(result);
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,9 +1,121 @@
-/*
-console.log('Hello World! form console')
+'use strict';
 
-let p: HTMLElement = document.getElementById('js-div');
+{
+  let hello = function (name: string, age: number): void {
+    console.log('Hello, ' + name + '(' + age + ')');
+  }
 
-p.addEventListener('click', function() {
-  p.textContent = '！';
-});
-*/
+  let name: string = "Taro";
+  let age: number = 25;
+
+  hello(name, age);
+}
+
+{
+  const person: any = {
+    name: ['Taro', 'Jiro', 'Subro'],
+    birth: 1994,
+    interests: {
+      sports: 'football',
+      music: 'rock',
+    },
+    getAllName: function(): void {
+      for(let i of this.name) {
+        console.log(i, this.name[i])
+      }
+    }
+  };
+
+  console.log(person.birth);
+  person.getAllName();
+}
+
+{
+  const todos: any[] = [
+    {
+      id: 1,
+      title: 'working',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'study',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'shopping',
+      completed: true,
+    }
+  ];
+
+  for(let i in todos) {
+    let todo = todos[i];
+    if(todo.completed === true) {
+      console.log(todo.id, todo.title);
+    }
+  }
+}
+
+{
+  let hello = function(callback: any): void {
+    console.log('hello ' + callback());
+  }
+
+  let getName = function (): string {
+    return 'Ichiro';
+  }
+
+  hello(getName);
+}
+
+{
+  const arry: string[] = ['Ichiro', 'Suzuki', 'Toyota', 'Nissan'];
+
+  arry.forEach(function(val, i, ary) {
+    console.log(val);
+  });
+}
+
+{
+  const arry: number[] = [1,2,3,4,5];
+
+  arry.reduce(function(accu: number, curr: number): number {
+    console.log(accu, curr);
+    return accu + curr;
+  });
+
+  const str: string = 'animation'
+  const strArry: string[] = str.split('');
+
+  console.log(strArry);
+
+  const result: string = strArry.reduce((accu: string, curr: string): string => {
+    return `${accu}<${curr}>`;
+  }, '');
+
+  console.log(result);
+}
+
+{
+  const str: string = 'animation';
+  const strArry: string[] = str.split('');
+
+  let tag = function(accu: string, curr: string): string {
+    return `${accu}<${curr}>`
+  }
+
+  let reduce = function(arry: string[], callback: any, defaultValue: string): string {
+    let accu: string = defaultValue;
+
+    for(let i: number = 0; i < arry.length; i++) {
+      let curr: string = arry[i];
+      accu = callback(accu, curr);
+    }
+
+    return accu
+  }
+
+  const result: string = reduce(strArry, tag, "");
+  console.log(result);
+}
