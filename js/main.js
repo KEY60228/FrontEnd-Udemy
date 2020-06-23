033699966cbed066a2b06578f3c5e8281854cd5c
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-06-23 20:09:35 +0900

    finished part.71 (Class)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 'use strict';
 {
-    var btn = document.querySelector('#btn');
-    var h1_1 = document.querySelector('#main-title');
-    var colorize = function () {
-        h1_1.style.color = 'red';
-    };
-    btn.addEventListener('click', colorize);
+    document.addEventListener('DOMContentLoaded', function () {
+        var ta = new TextAnimation_1('.animate-title');
+        var ta2 = new TextAnimation_1('.animate-title-2');
+        var btn = document.querySelector('#js-btn');
+        btn.addEventListener('click', function () {
+            ta.animate();
+            ta2.animate();
+        });
+    });
+    var TextAnimation_1 = /** @class */ (function () {
+        function TextAnimation(el) {
+            this.el = '';
+            this.chars = '';
+            this.el = document.querySelector(el);
+            this.chars = this.el.innerHTML.trim().split("");
+            this.el.innerHTML = this._splitText();
+        }
+        TextAnimation.prototype._splitText = function () {
+            return this.chars.reduce(function (acc, curr) {
+                curr = curr.replace(/\s+/, '&nbsp;');
+                return acc + "<span class=\"char\">" + curr + "</span>";
+            }, "");
+        };
+        TextAnimation.prototype.log = function () {
+            console.log(this.el);
+        };
+        TextAnimation.prototype.animate = function () {
+            this.el.classList.toggle('inview');
+        };
+        return TextAnimation;
+    }());
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,12 +1,40 @@
-'use strict'
+'use strict';
 
 {
-  const btn: HTMLElement = document.querySelector('#btn');
-  const h1: HTMLElement = document.querySelector('#main-title');
+  document.addEventListener('DOMContentLoaded', function() { 
+    const ta = new TextAnimation('.animate-title');
+    const ta2 = new TextAnimation('.animate-title-2');
+    const btn = document.querySelector('#js-btn');
 
-  const colorize: any = function () {
-    h1.style.color = 'red';
+    btn.addEventListener('click', function() {
+      ta.animate();
+      ta2.animate();
+    });
+  });
+
+  class TextAnimation {
+    el = '';
+    chars = '';
+
+    constructor(el) {
+      this.el = document.querySelector(el);
+      this.chars = this.el.innerHTML.trim().split("");
+      this.el.innerHTML = this._splitText();
+    }
+    
+    _splitText() {
+      return this.chars.reduce(function(acc, curr) {
+        curr = curr.replace(/\s+/, '&nbsp;');
+        return `${acc}<span class="char">${curr}</span>`;
+      }, "");
+    }
+
+    log() {
+      console.log(this.el);
+    }
+
+    animate() {
+      this.el.classList.toggle('inview');
+    }
   }
-
-  btn.addEventListener('click', colorize);
 }
