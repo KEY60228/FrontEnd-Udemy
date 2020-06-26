9349e8b46ff292b73b1f60d9847f0011e766a0b1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-06-26 22:13:10 +0900

    finished part.85 (hamburger menu)

--- js/main.js
+++ js/main.js
@@ -1,105 +1,25 @@
 'use strict';
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 {
-    var TextAnimation = /** @class */ (function () {
-        function TextAnimation(el) {
+    var MobileMenu = /** @class */ (function () {
+        function MobileMenu() {
             this.DOM = {};
-            this.DOM.el = el instanceof HTMLElement ? el : document.querySelector(el);
-            this.chars = this.DOM.el.innerHTML.trim().split("");
-            this.DOM.el.innerHTML = this._splitText();
+            this.DOM.btn = document.querySelector('.mobile-menu__btn');
+            this.DOM.cover = document.querySelector('.mobile-menu__cover');
+            this.DOM.container = document.querySelector('#global-container');
+            this.eventType = this._getEventType();
+            this._addEvent();
         }
-        TextAnimation.prototype._splitText = function () {
-            return this.chars.reduce(function (acc, curr) {
-                curr = curr.replace(/\s+/, '&nbsp;');
-                return acc + "<span class=\"char\">" + curr + "</span>";
-            }, "");
+        MobileMenu.prototype._getEventType = function () {
+            return window.ontouchstart ? 'touchstart' : 'click';
         };
-        TextAnimation.prototype.animate = function () {
-            this.DOM.el.classList.toggle('inview');
+        MobileMenu.prototype._toggle = function () {
+            this.DOM.container.classList.toggle('menu-open');
         };
-        return TextAnimation;
+        MobileMenu.prototype._addEvent = function () {
+            this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+            this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
+        };
+        return MobileMenu;
     }());
-    var TweenTextAnimation = /** @class */ (function (_super) {
-        __extends(TweenTextAnimation, _super);
-        function TweenTextAnimation(el) {
-            var _this = _super.call(this, el) || this;
-            _this.DOM.chars = _this.DOM.el.querySelectorAll('.char');
-            return _this;
-        }
-        TweenTextAnimation.prototype.animate = function () {
-            this.DOM.el.classList.add('inview');
-            this.DOM.chars.forEach(function (c, i) {
-                TweenMax.to(c, .6, {
-                    ease: Back.easeOut,
-                    delay: i * .05,
-                    startAt: { y: '-50%', opacity: 0 },
-                    y: '0%',
-                    opacity: 1
-                });
-            });
-        };
-        return TweenTextAnimation;
-    }(TextAnimation));
-}
-{
-    document.addEventListener('DOMContentLoaded', function () {
-        var cb = function (el, isIntersecting) {
-            if (isIntersecting) {
-                el.classList.add('inview');
-            }
-        };
-        var so = new ScrollObserver('.cover-slide', cb, null);
-    });
-}
-{
-    var ScrollObserver = /** @class */ (function () {
-        function ScrollObserver(els, cb, options) {
-            this.els = document.querySelectorAll(els);
-            var defaultOptions = {
-                root: null,
-                rootMargin: "0px",
-                threshold: 0,
-                once: true
-            };
-            this.cb = cb;
-            this.options = Object.assign(defaultOptions, options);
-            this.once = this.options.once;
-            this._init();
-        }
-        ScrollObserver.prototype._init = function () {
-            var _this = this;
-            var callback = function (entries, observer) {
-                var _this = this;
-                entries.forEach(function (entry) {
-                    if (entry.isIntersecting) {
-                        _this.cb(entry.target, true);
-                        if (_this.once) {
-                            observer.unobserve(entry.target);
-                        }
-                    }
-                    else {
-                        _this.cb(entry.target, false);
-                    }
-                });
-            };
-            this.io = new IntersectionObserver(callback.bind(this), this.options);
-            this.els.forEach(function (el) { return _this.io.observe(el); });
-        };
-        ScrollObserver.prototype.destroy = function () {
-            this.io.disconnect();
-        };
-        return ScrollObserver;
-    }());
+    new MobileMenu();
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,91 +1,29 @@
 'use strict';
+
 {
-  class TextAnimation {
-    constructor(el) {
+  class MobileMenu {
+    constructor() {
       this.DOM = {};
-      this.DOM.el = el instanceof HTMLElement ? el : document.querySelector(el);
-      this.chars = this.DOM.el.innerHTML.trim().split("");
-      this.DOM.el.innerHTML = this._splitText();
+      this.DOM.btn = document.querySelector('.mobile-menu__btn');
+      this.DOM.cover = document.querySelector('.mobile-menu__cover');
+      this.DOM.container = document.querySelector('#global-container');
+      this.eventType = this._getEventType();
+      this._addEvent();
     }
-    _splitText() {
-      return this.chars.reduce((acc, curr) => {
-        curr = curr.replace(/\s+/, '&nbsp;');
-        return `${acc}<span class="char">${curr}</span>`;
-      }, "");
+
+    _getEventType() {
+      return window.ontouchstart ? 'touchstart' : 'click';
     }
-    animate() {
-      this.DOM.el.classList.toggle('inview');
+
+    _toggle() {
+      this.DOM.container.classList.toggle('menu-open');
+    }
+
+    _addEvent() { 
+      this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+      this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
     }
   }
-  class TweenTextAnimation extends TextAnimation {
-    constructor(el) {
-      super(el);
-      this.DOM.chars = this.DOM.el.querySelectorAll('.char');
-    }
-    
-    animate() {
-      this.DOM.el.classList.add('inview');
-      this.DOM.chars.forEach((c, i) => {
-        TweenMax.to(c, .6, {
-          ease: Back.easeOut,
-          delay: i * .05,
-          startAt: { y: '-50%', opacity: 0},
-          y: '0%',
-          opacity: 1
-        });
-      });
-    }
-  }
-}
 
-{
-  document.addEventListener('DOMContentLoaded', function () {
-    const cb = function (el, isIntersecting) {
-      if (isIntersecting) {
-        el.classList.add('inview');
-      }
-    }
-    const so = new ScrollObserver('.cover-slide', cb, null);
-  });
-}
-
-{
-  class ScrollObserver {
-    constructor(els, cb, options) {
-      this.els = document.querySelectorAll(els);
-      const defaultOptions = {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0,
-        once: true,
-      };
-      this.cb = cb;
-      this.options = Object.assign(defaultOptions, options);
-      this.once = this.options.once;
-      this._init();
-    }
-
-    _init() {
-      const callback = function (entries, observer) {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            this.cb(entry.target, true);
-            if (this.once) {
-              observer.unobserve(entry.target);
-            }
-          } else {
-            this.cb(entry.target, false);
-          }
-        });
-      };
-
-      this.io = new IntersectionObserver(callback.bind(this), this.options);
-
-      this.els.forEach(el => this.io.observe(el));
-    }
-
-    destroy() {
-      this.io.disconnect();
-    }
-  }
+  new MobileMenu();
 }
