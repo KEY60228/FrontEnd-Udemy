5b8cd5b42c92672aa8cff4e0dee6e594e9431f13
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-06-27 23:21:18 +0900

    finished part.90 (hero slider)

--- js/main.js
+++ js/main.js
@@ -1,25 +1,46 @@
 'use strict';
 {
-    var MobileMenu = /** @class */ (function () {
-        function MobileMenu() {
-            this.DOM = {};
-            this.DOM.btn = document.querySelector('.mobile-menu__btn');
-            this.DOM.cover = document.querySelector('.mobile-menu__cover');
-            this.DOM.container = document.querySelector('#global-container');
-            this.eventType = this._getEventType();
-            this._addEvent();
+    document.addEventListener('DOMContentLoaded', function () {
+        var hero = new HeroSlider_1('.swiper-container');
+        hero.start({ delay: 2000 });
+        setTimeout(function () {
+            hero.stop();
+        }, 5000);
+    });
+    var HeroSlider_1 = /** @class */ (function () {
+        function HeroSlider(el) {
+            this.el = el;
+            this.swiper = this._initSwiper();
         }
-        MobileMenu.prototype._getEventType = function () {
-            return window.ontouchstart ? 'touchstart' : 'click';
+        HeroSlider.prototype._initSwiper = function () {
+            return new Swiper(this.el, {
+                // Optional parameters
+                // direction: 'vertical',
+                loop: true,
+                grabCursor: true,
+                effext: 'coverflow',
+                centeredSlides: true,
+                slidesPerView: 1,
+                speed: 1000,
+                breakpoints: {
+                    1024: {
+                        slidesPerView: 2
+                    }
+                }
+            });
         };
-        MobileMenu.prototype._toggle = function () {
-            this.DOM.container.classList.toggle('menu-open');
+        HeroSlider.prototype.start = function (options) {
+            if (options === void 0) { options = {}; }
+            options = Object.assign({
+                delay: 4000,
+                disableOnInteraction: false
+            }, options);
+            this.swiper.params.autoplay = options;
+            this.swiper.autoplay.start();
         };
-        MobileMenu.prototype._addEvent = function () {
-            this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
-            this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
+        HeroSlider.prototype.stop = function () {
+            this.swiper.autoplay.stop();
         };
-        return MobileMenu;
+        return HeroSlider;
     }());
-    new MobileMenu();
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,29 +1,50 @@
 'use strict';
 
 {
-  class MobileMenu {
-    constructor() {
-      this.DOM = {};
-      this.DOM.btn = document.querySelector('.mobile-menu__btn');
-      this.DOM.cover = document.querySelector('.mobile-menu__cover');
-      this.DOM.container = document.querySelector('#global-container');
-      this.eventType = this._getEventType();
-      this._addEvent();
+  document.addEventListener('DOMContentLoaded', function() {
+    const hero = new HeroSlider('.swiper-container');
+    hero.start({delay: 2000});
+    setTimeout(() => {
+      hero.stop();
+    }, 5000);
+  });
+
+  class HeroSlider {
+    constructor(el) {
+      this.el = el;
+      this.swiper = this._initSwiper();
     }
 
-    _getEventType() {
-      return window.ontouchstart ? 'touchstart' : 'click';
+    _initSwiper() {
+      return new Swiper (this.el, {
+        // Optional parameters
+        // direction: 'vertical',
+        loop: true,
+        grabCursor: true,
+        effext: 'coverflow',
+        centeredSlides: true,
+        slidesPerView: 1,
+        speed: 1000,
+        breakpoints: {
+          1024: {
+            slidesPerView: 2,
+          }
+        },
+      });
     }
 
-    _toggle() {
-      this.DOM.container.classList.toggle('menu-open');
+    start(options = {}) {
+      options = Object.assign({
+        delay: 4000,
+        disableOnInteraction: false,
+      }, options);
+
+      this.swiper.params.autoplay = options
+      this.swiper.autoplay.start();
     }
 
-    _addEvent() { 
-      this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
-      this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
+    stop() {
+      this.swiper.autoplay.stop();
     }
   }
-
-  new MobileMenu();
 }
