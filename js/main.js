9643e06cd215084b4f49209e07b8931d0ca075ad
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-06-29 17:08:47 +0900

    finished part.116 (Production - Done!)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,39 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-    var hero = new HeroSlider('.swiper-container');
-    hero.start();
-    var cb = function (el, isIntersecting) {
+    new Main();
+});
+var Main = /** @class */ (function () {
+    function Main() {
+        this.header = document.querySelector('.header');
+        this.sides = document.querySelectorAll('.side');
+        this._observers = [];
+        this._init();
+    }
+    Object.defineProperty(Main.prototype, "observers", {
+        get: function () {
+            return this._observers;
+        },
+        set: function (val) {
+            this._observers.push(val);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Main.prototype._init = function () {
+        new MobileMenu();
+        this.hero = new HeroSlider('.swiper-container');
+        Pace.on('done', this._paceDone.bind(this));
+    };
+    Main.prototype._paceDone = function () {
+        this._scrollInit();
+    };
+    Main.prototype._textAnimation = function (el, isIntersecting) {
         if (isIntersecting) {
-            var ta = new TextAnimation(el);
+            var ta = new TweenTextAnimation(el);
             ta.animate();
         }
     };
-    var so = new ScrollObserver('.animate-title', cb);
-    var _inviewAnimation = function (el, inview) {
+    Main.prototype._inviewAnimation = function (el, inview) {
         if (inview) {
             el.classList.add('inview');
         }
@@ -17,17 +41,46 @@ document.addEventListener('DOMContentLoaded', function () {
             el.classList.remove('inview');
         }
     };
-    var so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
-    var header = document.querySelector('.header');
-    ;
-    var _navAnimation = function (el, inview) {
+    Main.prototype._navAnimation = function (el, inview) {
         if (inview) {
-            header.classList.remove('triggered');
+            this.header.classList.remove('triggered');
         }
         else {
-            header.classList.add('triggered');
+            this.header.classList.add('triggered');
         }
     };
-    var so3 = new ScrollObserver('.nav-trigger', _navAnimation, { once: false });
-    new MobileMenu();
-});
+    Main.prototype._toggleSlideAnimation = function (el, inview) {
+        if (inview) {
+            this.hero.start();
+        }
+        else {
+            this.hero.stop();
+        }
+    };
+    Main.prototype._sideAnimation = function (el, inview) {
+        if (inview) {
+            this.sides.forEach(function (side) {
+                side.classList.add('inview');
+            });
+        }
+        else {
+            this.sides.forEach(function (side) {
+                side.classList.remove('inview');
+            });
+        }
+    };
+    Main.prototype._destroyObservers = function () {
+        this.observers.forEach(function (ob) {
+            ob.destroy();
+        });
+    };
+    Main.prototype._scrollInit = function () {
+        this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), { once: false });
+        this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation);
+        this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation);
+        this.observers = new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), { once: false });
+        this.observers = new ScrollObserver('.appear', this._inviewAnimation);
+        this.observers = new ScrollObserver('#main-content', this._sideAnimation.bind(this), { once: false, rootMargin: "-300px 0px" });
+    };
+    return Main;
+}());
